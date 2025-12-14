52ef767e93dfc70fc926fd8b1dfa0a55cfe999d9
aakanksha1dutta <aakanksha1dutta@gmail.com>
2025-12-14 17:00:55 -0500

    added additional default models

---
+++ default_model/mobilenetv2_torchscript.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7d2c1d4629d530a64fb6a4b523ec7731a018ea5c40f9853c150a5145e7bcd6d4
+size 9670331
---
+++ default_model/resnet18_torchscript.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f5e44394faf15af4ae3307e57997a04ba797ef8f36baaeedd1c853cc8e0b1364
+size 44984286
